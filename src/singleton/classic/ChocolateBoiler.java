b17246c38f58299eb959f6127daadd08810740db
Balsa <balsa.bane@gmail.com>
2019-02-08 13:52:56 +0100

    Adding code examples for singleton design pattern

---
+++ src/singleton/classic/ChocolateBoiler.java
@@ -0,0 +1,47 @@
+package singleton.classic;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+    public ChocolateBoiler chocolateBoiler;
+
+    private ChocolateBoiler(){
+        empty = true;
+        boiled = false;
+    }
+
+    public ChocolateBoiler getInstance(){
+        if (chocolateBoiler == null){
+            chocolateBoiler = new ChocolateBoiler();
+        }
+        return chocolateBoiler;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain(){
+        if(!isEmpty() && isBoiled()){
+            empty = true;
+        }
+    }
+
+    public void boil(){
+        if (!isEmpty() && !isBoiled()){
+            boiled = true;
+        }
+    }
+
+
+    public boolean isEmpty(){
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
